b3305d00f61ae2acdd0017a945c1870b93cc7b45
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-06-14 19:43:47 -0300

    sign up joi validation

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
+import signupRouter from "./routers/signup.js";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 dotenv.config()
 
+app.use(signupRouter);
 
 const {PORT} = process.env
 app.listen(PORT, () => {
