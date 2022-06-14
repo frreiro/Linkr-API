af47c27b54a724ca8e2b52765588b124de39b2ef
marcojr73 <marcojuniorengineer@gmail.com>
2022-06-14 19:12:46 -0300

    o usuário já consegue fazer o login

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
+import authRoutes from "./routers/authRouter.js";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 dotenv.config()
 
+app.use(authRoutes)
 
 const {PORT} = process.env
 app.listen(PORT, () => {
