f47f7d21ae7cd25876933a9666fad7b4072da6c2
marcojr73 <marcojuniorengineer@gmail.com>
2022-06-15 18:23:49 -0300

    estrutura inicial

---
+++ src/controllers/likeController.js
@@ -0,0 +1,12 @@
+// [] devo validar se o usuário enviou um token válido
+// [] devo receber na req um corpo com o id do post informado
+// [] devo associar esse token ao id do user
+// [] devo armazenar na tabela de postsLikes o idUser e o idpost
+// [] devo receceber na header o id do post como params
+// [] devo filtrar e armazenar quem curtiu aquele post 
+// [] devo retornar uma lista com os nomes de quem curtiu
+//
+
+export async function like(req, res){
+
+}
---
+++ src/routers/likesRouter.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const likesRouter = Router();
+
+likesRouter.post("/likes");
+
+export default likesRouter;
