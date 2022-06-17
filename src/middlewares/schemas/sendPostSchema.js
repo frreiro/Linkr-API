d0ddfdaddc72dd4fde4cd42e06ddb5632cee6ccc
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-06-17 17:01:46 -0300

    fix: missing schema line

--- src/middlewares/schemas/sendPostSchema.js
+++ src/middlewares/schemas/sendPostSchema.js
@@ -3,7 +3,7 @@ import joi from "joi"
 const sendPostSchema = joi.object({
     username: joi.string().required(),
     url: joi.string().uri().required(),
-    description: joi.string(),
+    description: joi.string().allow(""),
 })
 
 export default sendPostSchema
