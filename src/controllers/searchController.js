b91abdd0eff640258564c313acb3e9b6d08f5eed
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-06-17 23:46:48 -0300

    bug fix

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -3,7 +3,6 @@ import { userRepository } from "../repositories/userRepository.js";
 export const searchGet = async (req, res) => {
     let body = req.body;
     try{
-        console.log(body.question);
         const result = await userRepository.getUserByName(body.question);
         res.status(200).send(result.rows);
     } catch(e){
