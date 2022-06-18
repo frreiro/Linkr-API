3e2fffd199d280dd32474db3033691011acb8fb7
Lucas Viana <lucasvfv@gmail.com>
2022-06-18 14:54:43 -0300

    feat: returning tranding hashtags

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import authRoutes from './src/routers/authRouter.js';
 import postRouter from './src/routers/postRouter.js';
 import likesRouter from './src/routers/likesRouter.js';
 import timelineRoutes from './src/routers/timelineRoute.js';
+import hashtagsRouter from './src/routers/hashtagsRouter.js';
 
 const app = express();
 app.use(cors());
@@ -18,6 +19,7 @@ app.use(authRoutes);
 app.use(postRouter);
 app.use(likesRouter)
 app.use(timelineRoutes)
+app.use(hashtagsRouter)
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
--- src/routers/hashtagsRouter.js
+++ src/routers/hashtagsRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getPostsByHashtag } from './../controllers/hashtagsController.js';
+import { tokenExists } from '../middlewares/timelineMiddleware.js';
+import { getHashtag, getPostsByHashtag } from './../controllers/hashtagsController.js';
 
 const hashtagsRouter = Router();
 
 hashtagsRouter.get('/hashtags/:hashtag', getPostsByHashtag);
+hashtagsRouter.get('/hashtags', tokenExists, getHashtag)
 
 export default hashtagsRouter;
