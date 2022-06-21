f30b7d254642d377c5c45ad7a15aed2e3a41d640
Lucas Viana <lucasvfv@gmail.com>
2022-06-21 20:42:51 -0300

    feat: only user and user follower posts in timeline

--- src/repositories/repositoryTimeline.js
+++ src/repositories/repositoryTimeline.js
@@ -1,13 +1,15 @@
 import connectDB from "../config/database.js";
 
 const db = await connectDB();
-async function getPost(page) {
+async function getPost(page, userId) {
     const offset = `OFFSET ${page * 10}`
     return db.query(`
-    SELECT p.id, u.id as "userId", u."userName", u.image as "userImage", p.description as "postDescription", l.title, l.description, l.url, l.image, 0 as "retweetCount", p."createdAt", false as "isRetweet", null as "retweeterUsername" 
+    SELECT p.id, u.id as "userId", u."userName", u.image as "userImage", p.description as "postDescription", l.title, l.description, l.url, l.image, 0 as "retweetCount", p."createdAt", false as "isRetweet", null as "retweeterUsername"
     FROM posts p
     JOIN users u ON u.id = p."userId"
     JOIN "linkInfo" l ON l.id = p."linkId"
+    LEFT JOIN followers f ON f."followedId" = u.id
+    WHERE f."userId" = $1 OR u.id = $1
     UNION ALL 
     SELECT p.id, u1.id as "userId", u1."userName", u1.image as "userImage", p.description as "postDescription", l.title, l.description, l.url, l.image, r1."retweetCount", r."createdAt", true as "isRetweet", u2."userName" as "retweeterUsername"
     FROM posts p
@@ -21,10 +23,12 @@ async function getPost(page) {
     JOIN users u1 ON u1.id = p."userId"
     JOIN users u2 ON u2.id = r."userId"
     JOIN "linkInfo" l ON l.id = p."linkId"
+    LEFT JOIN followers f2 ON f2."followedId" = u1.id
+    WHERE f2."userId" = $1 OR u1.id = $1
     ORDER BY "createdAt" DESC
     ${offset}
     LIMIT 10
-    `);
+    `, [userId]);
 }
 
 async function getToken(token) {
