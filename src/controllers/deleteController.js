bedfa2eccc2888272b826017ad5fc6052f4fece9
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-06-24 15:27:59 -0300

    new delete controller

--- src/controllers/deleteController.js
+++ src/controllers/deleteController.js
@@ -2,15 +2,9 @@ import { likesRepository } from "../repositories/likesRepository.js";
 import { userRepository } from "../repositories/userRepository.js";
 
 export const deletePost = async (req, res) => {
-    const userId = res.locals.user.userId
     const postId = req.params.id
     try{
-        let validate = await userRepository.checkPostOwner(postId, userId);
-        if(validate.rows[0] === undefined){
-            res.sendStatus(401);
-        } else {
-            await likesRepository.deletePost(postId);
-        }
+        await likesRepository.deletePost(postId);
     } catch(e){
         res.sendStatus(500)
         console.log(e);
