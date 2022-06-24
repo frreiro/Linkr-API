0ffceed1a7b107afe9a4c8e7484727d367b3d835
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-06-23 22:56:56 -0300

    new search bar

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -5,9 +5,9 @@ export const searchGet = async (req, res) => {
     let body = req.body;
     let id = res.locals.userId;
     try{
-        const following = await followRepository.followingList(id)
+        const following = await followRepository.followingList(id);
         const result = await userRepository.getUserByName(body.question);
-        res.status(200).send(result.rows);
+        res.status(200).send(result.rows, following.rows);
     } catch(e){
         res.sendStatus(500);
         console.log(e);
