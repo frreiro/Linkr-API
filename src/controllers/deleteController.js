c5d2301dc717db3dcdb57c4a43fa892016593e09
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-06-24 15:32:38 -0300

    status send

--- src/controllers/deleteController.js
+++ src/controllers/deleteController.js
@@ -5,6 +5,7 @@ export const deletePost = async (req, res) => {
     const postId = req.params.id
     try{
         await likesRepository.deletePost(postId);
+        res.sendStatus(200);
     } catch(e){
         res.sendStatus(500)
         console.log(e);
