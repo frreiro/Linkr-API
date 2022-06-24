a2fcf6597ddfb96828b0d7d276a324543b5d54d1
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-06-23 22:56:04 -0300

    changing branchs

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,8 +1,11 @@
+import { followRepository } from "../repositories/followRepository.js";
 import { userRepository } from "../repositories/userRepository.js";
 
 export const searchGet = async (req, res) => {
     let body = req.body;
+    let id = res.locals.userId;
     try{
+        const following = await followRepository.followingList(id)
         const result = await userRepository.getUserByName(body.question);
         res.status(200).send(result.rows);
     } catch(e){
--- src/middlewares/searchMiddleware.js
+++ src/middlewares/searchMiddleware.js
@@ -2,9 +2,16 @@ import { searchSchema } from "./schemas/searchSchema.js";
 
 export const searchMiddleware = async (req, res, next) => {
     let body = req.body;
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '').trim();
+    if (!token) return res.sendStatus(401);
     let { error } = searchSchema.validate(body);
     if(!error){
-        next();
+        const { rows } = await repositoryTimeline.getToken(token)
+        const { userId } = rows[0]
+        if (!userId) return res.status(401).send("Invalid token.")
+        res.locals.userId = userId
+        next()
     } else {
         res.sendStatus(422);
     }
